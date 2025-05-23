f27eff39730d6eb1a2087cb0d07946aa94915640
Hasbi54 <hasbi23ti@mahasiswa.pcr.ac.id>
2025-05-23 10:00:44 +0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./assets/tailwind.css";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
+// import Sidebar from "./components/Sidebar";
+// import Header from "./components/Header";
+// import Dashboard from "./pages/Dashboard";
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
